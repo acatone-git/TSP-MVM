200709e837409d35fb0d524c905810c6c9ff0be3
acatone-git <67967964+acatone-git@users.noreply.github.com>
2024-05-20 07:10:54 +0100

    Add files via upload

---
+++ TSP-MVM.bat
@@ -0,0 +1,26 @@
+@echo off
+echo #--------------------------------------#
+echo - TrimUI Smart Pro - Music Video Maker -
+echo #--------------------------------------#
+echo Converting Cover Image...
+for %%f in (*.jpg *.png) do (
+    ffmpeg -loglevel panic -i "%%f" -y -vf "scale=640:360:force_original_aspect_ratio=decrease,pad=640:360:-1:-1:color=black" "cover.jpg"
+)
+echo Converting MP3 to MP4...
+for %%G in (*.mp3) do (
+    ffmpeg -loglevel panic -y -loop 1 -i cover.jpg -i "%%G" -c:v libx264 -tune stillimage -c:a mp3 -pix_fmt yuv420p -shortest "%%~nG.mp4"
+)
+echo #--------------------------------------#
+echo ---------- Final Cleanup ---------------
+echo #--------------------------------------#
+REM Prompt the user
+set /p "deleteFiles=Keep only the MP4 files (Y/N): "
+if /i "%deleteFiles%"=="Y" (
+    del /S *.mp3
+    del /S *.jpg
+    del /S *.png
+    del /S *.exe
+    del /S *.bat
+) else (
+    exit
+)
